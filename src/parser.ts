3bed1f449c709d53ef5c7da55ccea53d5dac61e3
pywd <yuki.wd7@gmail.com>
2021-02-11 13:35:02 +0900

    immutableな感じに..?

--- src/parser.ts
+++ src/parser.ts
@@ -2,37 +2,54 @@ import { Block } from "./types";
 
 const FENCED_CODE_BLOCK_REGEX = /^(?<marker> {0,3}(?:`{3,}|~{3,})) {0,}(?<info>.+)?$/;
 
+interface State {
+  ast: Block[];
+  openBlock: Block | undefined;
+}
+
 class Parser {
   private blocks: Block[] = [];
   private openBlock: Block | undefined;
   public parse(markdown: string): Block[] {
     const lines = markdown.split(/[\r\n]/);
     for (const line of lines) {
-      const block = this.parseLine(line);
-      if (block != null) {
-        this.openBlock = block;
-      }
+      const state = this.parseLine(line, {
+        ast: this.blocks,
+        openBlock: this.openBlock,
+      });
+      this.blocks = state.ast;
+      this.openBlock = state.openBlock;
     }
-    this.closeBlock();
-    return this.blocks;
+    const result = this.close({
+      ast: this.blocks,
+      openBlock: this.openBlock,
+    });
+    return result.ast;
   }
 
-  private parseLine(line: string): Block | undefined {
+  private parseLine(line: string, state: State): State {
     if (line.trim().length === 0) {
-      if (this.openBlock?.type === "indented-code-block") {
+      if (state.openBlock?.type === "indented-code-block") {
         return {
-          ...this.openBlock,
-          text: [this.openBlock.text, line.replace(/^ {0,4}/, "")].join("\n"),
+          ...state,
+          openBlock: {
+            ...state.openBlock,
+            text: [state.openBlock.text, line.replace(/^ {0,4}/, "")].join(
+              "\n"
+            ),
+          },
         };
       }
-      if (this.openBlock?.type === "fenced-code-block") {
+      if (state.openBlock?.type === "fenced-code-block") {
         return {
-          ...this.openBlock,
-          text: [this.openBlock.text, line].join("\n"),
+          ...state,
+          openBlock: {
+            ...state.openBlock,
+            text: [state.openBlock.text, line].join("\n"),
+          },
         };
       }
-      this.closeBlock();
-      return undefined;
+      return this.close(state);
     }
 
     // Setext headings
@@ -42,140 +59,182 @@ class Parser {
       this.openBlock?.type === "paragraph"
     ) {
       return {
-        type: "heading",
-        level: setextHeadingMatch.groups.level[0] === "-" ? 2 : 1,
-        text: this.openBlock.text,
+        ...state,
+        openBlock: {
+          type: "heading",
+          level: setextHeadingMatch.groups.level[0] === "-" ? 2 : 1,
+          text: this.openBlock.text,
+        },
       };
     }
 
     // Thematic breaks
     if (line.match(/^ {0,3}([-_*] {0,}){3,}$/)) {
-      this.closeBlock();
       return {
-        type: "thematic-break",
+        ...this.close(state),
+        openBlock: {
+          type: "thematic-break",
+        },
       };
     }
 
     // ATX headings
     const atxHeadingsMatch = line.match(/^ {0,3}(?<atx>#{1,6})(?<text> .*|$)/);
     if (atxHeadingsMatch?.groups != null) {
-      this.closeBlock();
+      if (state.openBlock != null) {
+        return {
+          ...this.close(state),
+          openBlock: {
+            type: "heading",
+            level: atxHeadingsMatch.groups.atx.length as 1 | 2 | 3 | 4 | 5 | 6,
+            text: atxHeadingsMatch.groups.text
+              .replace(/ #{1,} {0,}$/, "")
+              .trim(),
+          },
+        };
+      }
       return {
-        type: "heading",
-        level: atxHeadingsMatch.groups.atx.length as 1 | 2 | 3 | 4 | 5 | 6,
-        text: atxHeadingsMatch.groups.text.replace(/ #{1,} {0,}$/, "").trim(),
+        ...state,
+        openBlock: {
+          type: "heading",
+          level: atxHeadingsMatch.groups.atx.length as 1 | 2 | 3 | 4 | 5 | 6,
+          text: atxHeadingsMatch.groups.text.replace(/ #{1,} {0,}$/, "").trim(),
+        },
       };
     }
 
     // Indented code blocks
     if (
       line.match(/^ {4,}.+/) &&
-      this.openBlock?.type !== "paragraph" &&
-      this.openBlock?.type !== "fenced-code-block"
+      state.openBlock?.type !== "paragraph" &&
+      state.openBlock?.type !== "fenced-code-block"
     ) {
       const text = line.replace(/^ {0,4}/, "");
-      if (this.openBlock?.type === "indented-code-block") {
+      if (state.openBlock?.type === "indented-code-block") {
         return {
-          type: "indented-code-block",
-          text: [this.openBlock.text, text].join("\n"),
+          ...state,
+          openBlock: {
+            type: "indented-code-block",
+            text: [state.openBlock.text, text].join("\n"),
+          },
         };
       }
-      this.closeBlock();
       return {
-        type: "indented-code-block",
-        text: text,
+        ...this.close(state),
+        openBlock: {
+          type: "indented-code-block",
+          text: text,
+        },
       };
     }
 
     const fencedCodeBlockMatch = line.match(FENCED_CODE_BLOCK_REGEX);
     if (fencedCodeBlockMatch && fencedCodeBlockMatch.groups) {
       // fenced code block終了
-      if (this.openBlock?.type === "fenced-code-block") {
+      if (state.openBlock?.type === "fenced-code-block") {
         const endMarker = fencedCodeBlockMatch.groups.marker.replace(
           /^ {0,3}/,
           ""
         );
-        const startMarker = this.openBlock.marker.replace(/^ {0,3}/, "");
+        const startMarker = state.openBlock.marker.replace(/^ {0,3}/, "");
         // markerが同じとき終了
         if (
           fencedCodeBlockMatch.groups.info == null &&
           endMarker[0] === startMarker[0] &&
           endMarker.length >= startMarker.length
         ) {
-          this.closeBlock();
-          return undefined;
+          return this.close(state);
         }
       } else {
-        this.closeBlock();
         return {
-          type: "fenced-code-block",
-          marker: fencedCodeBlockMatch.groups.marker,
-          text: "",
-          info: fencedCodeBlockMatch.groups.info,
+          ...this.close(state),
+          openBlock: {
+            type: "fenced-code-block",
+            marker: fencedCodeBlockMatch.groups.marker,
+            text: "",
+            info: fencedCodeBlockMatch.groups.info,
+          },
         };
       }
     }
 
     // Paragraphs
-    if (this.openBlock?.type === "paragraph") {
+    if (state.openBlock?.type === "paragraph") {
       return {
-        type: "paragraph",
-        text: [this.openBlock.text, line.trim()].join("\n"),
+        ...state,
+        openBlock: {
+          type: "paragraph",
+          text: [state.openBlock.text, line.trim()].join("\n"),
+        },
       };
     }
-    if (this.openBlock?.type === "fenced-code-block") {
-      const indent = this.openBlock.marker.match(/^(?<indent> {1,3})/);
+    if (state.openBlock?.type === "fenced-code-block") {
+      const indent = state.openBlock.marker.match(/^(?<indent> {1,3})/);
       const indentLength =
         indent !== null && indent.groups != null
           ? indent.groups.indent.length
           : 0;
       return {
-        ...this.openBlock,
-        text: [
-          this.openBlock.text,
-          line.replace(new RegExp(`^ {0,${indentLength}}`), ""),
-        ].join("\n"),
+        ...state,
+        openBlock: {
+          ...state.openBlock,
+          text: [
+            state.openBlock.text,
+            line.replace(new RegExp(`^ {0,${indentLength}}`), ""),
+          ].join("\n"),
+        },
       };
     }
-    this.closeBlock();
     return {
-      type: "paragraph",
-      text: line.trim(),
+      ...this.close(state),
+      openBlock: {
+        type: "paragraph",
+        text: line.trim(),
+      },
     };
   }
 
-  private closeBlock() {
-    if (this.openBlock) {
-      if (this.openBlock.type === "indented-code-block") {
-        const newText = this.openBlock.text
-          .split("\n")
-          .filter((v, i, array) => {
-            if ((i === 0 || i === array.length - 1) && v.trim().length < 1) {
-              return false;
-            }
-            return true;
-          })
-          .join("\n");
-        this.blocks = this.blocks.concat({
-          ...this.openBlock,
-          text: newText,
-        });
-      } else if (this.openBlock.type === "fenced-code-block") {
-        const newText = this.openBlock.text
-          .split("\n")
-          .filter((v, i) => {
-            return i !== 0;
-          })
-          .join("\n");
-        this.blocks = this.blocks.concat({
-          ...this.openBlock,
-          text: newText,
-        });
-      } else {
-        this.blocks = this.blocks.concat(this.openBlock);
-      }
-      this.openBlock = undefined;
+  private close(state: State): State {
+    if (state.openBlock == null) {
+      return state;
     }
+    if (state.openBlock.type === "indented-code-block") {
+      const newText = state.openBlock.text
+        .split("\n")
+        .filter((v, i, array) => {
+          if ((i === 0 || i === array.length - 1) && v.trim().length < 1) {
+            return false;
+          }
+          return true;
+        })
+        .join("\n");
+      return {
+        ast: state.ast.concat({
+          ...state.openBlock,
+          text: newText,
+        }),
+        openBlock: undefined,
+      };
+    }
+    if (state.openBlock.type === "fenced-code-block") {
+      const newText = state.openBlock.text
+        .split("\n")
+        .filter((v, i) => {
+          return i !== 0;
+        })
+        .join("\n");
+      return {
+        ast: state.ast.concat({
+          ...state.openBlock,
+          text: newText,
+        }),
+        openBlock: undefined,
+      };
+    }
+    return {
+      ast: state.ast.concat(state.openBlock),
+      openBlock: undefined,
+    };
   }
 }
 
